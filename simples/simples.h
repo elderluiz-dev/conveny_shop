95a262d25703537f14356d685547dc27e80b9a89
athlas <elderluiz72@gmail.com>
2026-09-06 22:47:57 -0300

    feat: adding simple list funcs

--- simples/simples.h
+++ simples/simples.h
@@ -12,4 +12,13 @@ typedef struct lista_pereciveis
 {
     p_perecivel *cabeca;
     int tamanho;
-};
+} lista_pereciveis;
+
+p_perecivel* criar_p_perecivel(p_perecivel novo);
+lista_pereciveis* criar_lista_pereciveis();
+lista_pereciveis* adicionar_perecivel(p_perecivel novo, lista_pereciveis *lista);
+void exibir_pereciveis(p_perecivel* atual);
+lista_pereciveis* remover_perecivel(lista_pereciveis* lista, int id);
+lista_pereciveis* esvaziar_pereciveis(lista_pereciveis* lista);
+lista_pereciveis* editar_qtd_perecivel(lista_pereciveis* lista, int id);
+void buscar_perecivel(lista_pereciveis* lista, char *substr);
