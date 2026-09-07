9f1e3a04cb5150c3161b9ca65d7cb2421fa197bb
santtos0x1 <ruanrodrigues393@gmail.com>
2026-09-07 19:14:32 -0300

    Feat: adding insertion at circular linked list

--- circular/circular.h
+++ circular/circular.h
@@ -1,3 +1,5 @@
+#pragma once
+
 typedef struct promo
 {
     int id;
@@ -10,6 +12,19 @@ typedef struct promo
 
 typedef struct lista_promo
 {
-    p_promo *cabeca;
+    p_promo *cauda;
     int tamanho;
-};
+} lista_promo;
+
+void insere_cabeca(p_promo **cauda, p_promo novo_produto, lista_promo *lista);
+void insere_cauda(p_promo **cauda, p_promo novo_produto, lista_promo *lista);
+
+void remove_inicio();
+void remove_fim();
+void remove_id();
+
+p_promo busca_nome();
+
+void atualiza_quantidade();
+
+void limpa_lista();
