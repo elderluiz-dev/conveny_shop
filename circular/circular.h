bff1294ddc98b067a8da00b070369220e24fc4b5
santtos0x1 <ruanrodrigues393@gmail.com>
2026-09-10 16:25:07 -0300

    Feat: adding free and show list functionalities

--- circular/circular.h
+++ circular/circular.h
@@ -27,8 +27,8 @@ void busca_nome(p_promo *cauda, char *nome);
 
 void atualiza_quantidade(p_promo **cauda, int id, int nova_quantidade);
 
-void exibe_lista(p_promo *cauda);
+void exibe_lista(p_promo **cauda);
 
-int contar_lista(p_promo *cauda);
+int conta_lista(p_promo *cauda);
 
-void limpa_lista();
+void limpa_lista(p_promo **cauda, lista_promo *lista);
